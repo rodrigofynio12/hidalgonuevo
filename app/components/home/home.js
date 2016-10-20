549b92dce8a32cac198dd6434ff3584b2c981925
rodrigofynio12 <hooligansvigo@hotmail.com>
2016-10-20 12:16:58 -0500

    acomodando todo

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -3,45 +3,8 @@ angularRoutingApp.controller('ControladorPrincipal', function($scope) {
 	$scope.message = 'home';
     
     
- 
-$scope.rodrigo = function()
 
-{
-    var valortam= $('.imagen1').width();
-    
-    var valor =   $('.imagen').css('left');
-    
-    var nuevo = parseFloat(valor);
-    nuevo= Math.round(nuevo*100)/100;
-    
-    nuevo= Math.round(nuevo);
-    nuevo=nuevo*-1;
-    
-    var rodrigo = nuevo/valortam;
-    
-    
-    
-    if(rodrigo==-Infinity)
-        {
-            $('.imagen').css('margin-left','100%').delay(100).$('.imagen').css('margin-left','200%').delay(100).css('margin-left','200%');
-            
-        }
-     if(rodrigo==1)
-        {
-            $('.imagen').css('margin-left','100%').delay(100).$('.imagen').css('margin-left','200%').delay(100).css('margin-left','200%');
-        }
-    
-     if(rodrigo==2)
-        {
-            $('.imagen').css('margin-left','100%').delay(100).$('.imagen').css('margin-left','200%').delay(100).css('margin-left','200%');
-        }
-    // 
-    
-    
-    
-    
-}
-  
+   
         
     
 
